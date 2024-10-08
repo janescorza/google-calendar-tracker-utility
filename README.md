6df3aa4a4d6431a55c35434851f03065815c29ca
Jan <jan@Jans-MacBook-Pro.local>
2024-10-08 18:01:52 +0200

    chore: introduce commitizen to the commit structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # google-calendar-tracker-utility
+
 Utility to ease the way I create events on my calendar
--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,13 +1,11 @@
-import { render } from "@testing-library/react-native";
-import React from "react";
-import App from "../App";
+import { render } from '@testing-library/react-native';
+import React from 'react';
+import App from '../App';
 
-describe("App", () => {
-  it("renders correctly", () => {
+describe('App', () => {
+  it('renders correctly', () => {
     const { getByText } = render(<App />);
-    const welcomeText = getByText(
-      "Hello there!!"
-    );
+    const welcomeText = getByText('Hello there!!');
     expect(welcomeText).toBeTruthy();
   });
 });
