384e8ca169a1872c79e5edd9f7636c83c108fb4a
Jan <jan@Jans-MacBook-Pro.local>
2024-10-09 12:57:59 +0200

    feat: introduce working google signin setup
    adds a woaking setup to login with google and does all setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+import { useEffect, useState } from "react";
+
+
 
 export default function App() {
+  const [error, setError] = useState();
+  const [userInfo, setUserInfo] = useState();
+
+  const configureGoogleSignIn = () => {
+    GoogleSignin.configure({
+      webClientId:
+        "843874562806-sihdthikal5qmefrjtivpauio35q8elg.apps.googleusercontent.com",
+      iosClientId:
+        "843874562806-ei87dghk76loc13vrgc96vbu2a0kffm9.apps.googleusercontent.com",
+    });
+  };
+
+  useEffect(() => {
+    configureGoogleSignIn();
+  });
+
+  const signIn = async () => {
+    console.log("Pressed sign in");
+
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      setUserInfo(userInfo);
+      setError();
+    } catch (e) {
+      setError(e);
+    }
+  };
+
+  const logout = () => {
+    setUserInfo(undefined);
+    GoogleSignin.revokeAccess();
+    GoogleSignin.signOut();
+  };
+
+
+
   return (
     <View style={styles.container}>
       <Text>Hello there!!</Text>
+      {userInfo && <Text>{JSON.stringify(userInfo.data.user)}</Text>}
+      {userInfo ? (
+        <Button title="Logout" onPress={logout} />
+      ) : (
+        <GoogleSigninButton
+          size={GoogleSigninButton.Size.Standard}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={signIn}
+        />
+      )}
       <StatusBar style="auto" />
     </View>
   );
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # google-calendar-tracker-utility
 
 Utility to ease the way I create events on my calendar
+
+## Useful commands
+Run `eas build --profile development --platform android` to create a dev build if I change native code.
+Run `npx expo start --dev-client` to start the dev server
