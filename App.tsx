2c7d8b9390951232604b4e1d24fa26f9766f1ff9
Jan <jan@Jans-MacBook-Pro.local>
2024-11-06 00:30:16 +0100

    fix(icons): change app icon and fix top margin

--- App.tsx
+++ App.tsx
@@ -1,32 +1,51 @@
 // App.tsx
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Platform,
+} from 'react-native';
 import CalendarEventsManager from './src/components/CalendarEventsManager';
 import { colors, typography, layout, spacing } from './src/styles/theme';
 
 const App: React.FC = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>The Calendarinator</Text>
-        <Text style={styles.subtitle}>
-          My utility for event-base time tracking
-        </Text>
-      </View>
-      <CalendarEventsManager />
-    </SafeAreaView>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={colors.background}
+        translucent
+      />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <Text style={styles.title}>The Calendarinator</Text>
+            <Text style={styles.subtitle}>
+              My utility for event-base time tracking
+            </Text>
+          </View>
+          <CalendarEventsManager />
+        </View>
+      </SafeAreaView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     ...layout.container,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  content: {
+    flex: 1,
   },
   header: {
     padding: spacing.medium,
     borderBottomWidth: 1,
     borderBottomColor: colors.surface,
-    marginTop: 32,
   },
   title: {
     ...typography.title,
